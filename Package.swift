00d56e178122b3e7286ed72aaf8df87278ec1864
Rui Costa <rui.pfcosta@gmail.com>
2019-04-30 08:29:41 +0100

    Added xcodeproj as a dependency.

--- Package.swift
+++ Package.swift
@@ -9,15 +9,12 @@ let package = Package(
         .executable(name: "bump", targets: ["Bump"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.7.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Bump",
-            dependencies: []),
+            dependencies: ["xcodeproj"]),
         .testTarget(
             name: "BumpTests",
             dependencies: ["Bump"]),
--- Sources/Bump/Bump Processors/Xcode.swift
+++ Sources/Bump/Bump Processors/Xcode.swift
@@ -1,4 +1,5 @@
 import Foundation
+import xcodeproj
 
 protocol BumpProcessor {
     func run(versionUpdater: VersionUpdater, remainingParts: [String]) throws
